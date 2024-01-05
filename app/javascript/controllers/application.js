0c0a3d9a53b879fab3abe4fe4e29f2b54c993a0a
Alice Bernaud <alice.bernaud@gmail.com>
2024-01-05 16:08:05 +0100

    navbar et form devise

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,6 @@
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
