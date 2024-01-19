5e95f0cb495fcfc938780c61391f2b220f76bad7
Alice Bernaud <alice.bernaud@gmail.com>
2024-01-19 12:08:44 +0100

    responsive

--- app/javascript/controllers/display_controller.js
+++ app/javascript/controllers/display_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="display"
 export default class extends Controller {
-  static targets = ["textarea", "modif", "regen"]
+  static targets = ["textarea", "modif", "regen", "view"]
 
   connect() {
+    this.viewTarget.classList.remove("decalage");
   }
 
   fireArea(event) {
@@ -12,8 +13,8 @@ export default class extends Controller {
 
     this.modifTarget.classList.toggle("d-none");
     this.regenTarget.classList.toggle("d-none");
-
     this.textareaTarget.classList.toggle("d-none");
+    this.viewTarget.classList.add("decalage");
   }
 
   cancelArea(event) {
@@ -21,6 +22,8 @@ export default class extends Controller {
     this.modifTarget.classList.toggle("d-none");
     this.regenTarget.classList.toggle("d-none");
     this.textareaTarget.classList.toggle("d-none");
+    this.viewTarget.classList.remove("decalage");
   }
+
 
 }
