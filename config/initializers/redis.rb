c30d85662ac4701129d10e0c6f675a0908e5f8a2
adam lamouri <adamlamouri.pro@gmail.com>
2024-01-21 19:32:31 +0100

    change redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -10,3 +10,9 @@ if url
     config.redis = { url: url }
   end
 end
+
+
+if ENV["REDISCLOUD_URL"]
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
