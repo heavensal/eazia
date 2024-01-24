fdbb5337c72fd2e8ebb26bd45221d8de11a17100
adam lamouri <adamlamouri.pro@gmail.com>
2024-01-24 23:35:23 +0100

    photo job

--- app/jobs/photo_job.rb
+++ app/jobs/photo_job.rb
@@ -4,15 +4,7 @@ class PhotoJob < ApplicationJob
   def perform(post)
     ai_api_service = AiApiService.new(post.user)
     post.pictures_generated.times do
-      photo = ai_api_service.image(post) # Retourne la nouvelle photo attachée
-
-      # Diffuser le Turbo Stream pour ajouter l'image à la vue
-      Turbo::StreamsChannel.broadcast_append_to(
-        post,
-        target: "images",
-        partial: "posts/image",
-        locals: { photo: photo }
-      )
+      photo = ai_api_service.image(post)
     end
   end
 end
