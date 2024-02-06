ba6db50835470d94e38de1c887f6d19fbb3b5306
adam lamouri <adamlamouri.pro@gmail.com>
2024-02-06 02:04:08 +0100

    publish

--- app/services/fb_api_service.rb
+++ app/services/fb_api_service.rb
@@ -23,8 +23,9 @@ class FbApiService
 
   # créer un container pour le futur post insta
   def new_container(post)
+    caption = URI.encode_www_form_component(post.gpt_creation.description)
     request = faraday_fb
-    response = request.post("https://graph.facebook.com/v19.0/#{post.user.ig_page}/media?image_url=#{post.photos.first.key}&caption=#{post.gpt_creation.description}&access_token=#{post.user.token}")
+    response = request.post("https://graph.facebook.com/v19.0/#{post.user.ig_page}/media?image_url=https://res.cloudinary.com/dhsr2pymr/image/upload/v1/production/#{post.photos.first.key}&caption=#{caption}&access_token=#{post.user.token}")
     data = JSON.parse(response.body)
     return data['id']
   end
