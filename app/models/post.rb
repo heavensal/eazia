861e835404a5a70530486c512f4df5ca6b5ba98a
adam lamouri <adamlamouri.pro@gmail.com>
2024-02-14 02:14:57 +0100

    enfin reussi pour les photos manque le carrousel

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one :gpt_creation, dependent: :destroy
   has_many :dalle3_images, dependent: :destroy
-  has_many_attached :photos,
-    after_add: :broadcast_photos
+  has_many_attached :photos
+  after_update_commit :broadcast_photos
 
   validates :prompt, presence: :true
   validates :pictures_generated, inclusion: { in: 0..5 }
@@ -14,6 +14,12 @@ class Post < ApplicationRecord
       target: "photos",
       partial: "posts/photos",
       locals: { post: self }
+    )
+    broadcast_update_to(
+      "post_#{self.id}",
+      target: "insta-photos",
+      partial: "posts/insta-photos",
+      locals: { images: self.photos }
     )
   end
 end
