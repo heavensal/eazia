811258b9340d5a741b7d01f1f7c57bcc1a3cc668
adam lamouri <adamlamouri.pro@gmail.com>
2024-02-14 14:19:52 +0100

    changement

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,9 +58,12 @@ Rails.application.configure do
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
+  config.active_job.queue_adapter = :sidekiq
+
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
