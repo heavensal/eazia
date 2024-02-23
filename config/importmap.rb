611b7971269c8d7a0cb17ebd9f585dcc2363e108
adam lamouri <adamlamouri.pro@gmail.com>
2024-02-23 19:33:23 +0100

    importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js"
-pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
-pin "@rails/actioncable", to: "actioncable.esm.js"
-pin_all_from "app/javascript/channels", under: "channels"
