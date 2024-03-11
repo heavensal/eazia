946fbf1899c94d6368ada0aed52bab62ca54aa2c
Alice Bernaud <alice.bernaud@gmail.com>
2024-03-11 15:14:08 +0100

    responsive in progress

--- app/javascript/controllers/autorisation_form_controller.js
+++ app/javascript/controllers/autorisation_form_controller.js
@@ -4,17 +4,18 @@ export default class extends Controller {
   static targets = ["input", "error", "checkbox", "myErrorMessage", "prompt"]
 
   connect() {
-    // console.log('Autorisation form connected');
+    // Ajout des écouteurs existants
     this.inputTargets.forEach(input => {
       input.addEventListener('blur', this.validateField.bind(this));
     });
     this.element.addEventListener('submit', this.validateForm.bind(this));
-
     if (this.hasCheckboxTarget) {
       this.checkboxTarget.addEventListener('change', this.validateForm.bind(this));
     }
-
     this.submitButton = this.element.querySelector('.btn-inscription') || this.element.querySelector('.main-bouton');
+
+    // Ajouter un nouvel écouteur d'événements pour le champ prompt
+    this.promptTarget.addEventListener('input', this.validatePrompt.bind(this));
   }
 
   validateField(event) {
@@ -62,20 +63,16 @@ export default class extends Controller {
   }
 
   validatePrompt() {
-    const promptText = this.promptTarget.value;
-    const isValid = promptText.length >= 20;
-    const submitButton = this.element.querySelector('.btn-valider');
+  const isPromptValid = this.promptTarget.value.length >= 20;
 
-    if (!isValid) {
-      // Désactiver le bouton de soumission et afficher un message d'erreur
-      submitButton.disabled = true;
-      this.errorTarget.classList.remove('d-none');
-      this.errorTarget.textContent = 'La description doit contenir au moins 20 caractères.';
-    } else {
-      // Activer le bouton de soumission et cacher le message d'erreur
-      // submitButton.disabled = false;
-      this.errorTarget.classList.add('d-none');
-    }
+  if (!isPromptValid) {
+    this.submitButton.disabled = true;
+    this.errorTarget.classList.remove('d-none');
+    this.errorTarget.textContent = 'La description doit contenir au moins 20 caractères.';
+  } else {
+    this.submitButton.disabled = false;
+    this.errorTarget.classList.add('d-none');
   }
 
+}
 }
