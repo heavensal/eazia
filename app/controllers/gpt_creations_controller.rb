bbabab0a9f8179be5fe52395e8a649d3f113606e
adam lamouri <adamlamouri.pro@gmail.com>
2024-03-16 18:19:48 +0100

    turbo render

--- app/controllers/gpt_creations_controller.rb
+++ app/controllers/gpt_creations_controller.rb
@@ -15,18 +15,12 @@ class GptCreationsController < ApplicationController
 
   def recreate
     @gpt_creation = @post.gpt_creation
-    ai_api_service = AiApiService.new(@post.user)
-    ai_api_service.work_2(@post)
-    respond_to do |f|
-      f.turbo_stream do
-        render turbo_stream: [
-          turbo_stream.replace(@gpt_creation, partial: "gpt_creations/recreate", locals: { gpt_creation: @gpt_creation }),
-          turbo_stream.replace("insta-#{@gpt_creation.id}" , partial: "gpt_creations/insta-description", locals: { gpt_creation: @gpt_creation, description: @gpt_creation.description })
-        ]
-      end
-      f.html do
-        redirect_to post_path(@post)
-      end
+    begin
+      ai_api_service = AiApiService.new(@post.user)
+      ai_api_service.work_2(@post)
+      turbo_render(@post, @gpt_creation)
+    rescue
+      redirect_to post_path(@post), alert: "Suite à une erreur de l'IA, la description de votre brouillon n'a pas pu être recréée. Merci de réessayer ultérieurement."
     end
   end
 
