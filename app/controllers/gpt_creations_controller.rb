7bd1d4006307a2c1cc10cb0531f9ff484770ff26
adam lamouri <adamlamouri.pro@gmail.com>
2024-03-16 18:16:16 +0100

    turbo render description

--- app/controllers/gpt_creations_controller.rb
+++ app/controllers/gpt_creations_controller.rb
@@ -7,7 +7,7 @@ class GptCreationsController < ApplicationController
 
   def update
     if @gpt_creation.update(description_params)
-      redirect_to post_path(@post), notice: 'Description mise à jour avec succès.'
+      turbo_render(@post, @gpt_creation)
     else
       redirect_to post_path(@post), status: :unprocessable_entity, notice: 'Erreur lors de la mise à jour de la description. Merci de réessayer.'
     end
@@ -19,24 +19,40 @@ class GptCreationsController < ApplicationController
     ai_api_service.work_2(@post)
     respond_to do |f|
       f.turbo_stream do
-        flash.now[:notice] = 'Nouvelle description en cours de création. Merci de patienter quelques instants.'
-        render turbo_stream: turbo_stream.replace(@gpt_creation, partial: "gpt_creations/recreate", locals: { gpt_creation: @gpt_creation })
+        render turbo_stream: [
+          turbo_stream.replace(@gpt_creation, partial: "gpt_creations/recreate", locals: { gpt_creation: @gpt_creation }),
+          turbo_stream.replace("insta-#{@gpt_creation.id}" , partial: "gpt_creations/insta-description", locals: { gpt_creation: @gpt_creation, description: @gpt_creation.description })
+        ]
       end
-      f.html {
-        flash[:notice] = 'Nouvelle description en cours de création. Merci de patienter quelques instants.'
+      f.html do
         redirect_to post_path(@post)
-        flash[:notice] = 'Nouvelle description créée avec succès.' } # Fallback pour les navigateurs sans prise en charge de Turbo
+      end
     end
   end
 
   private
 
-  def description_params
-    params.require(:gpt_creation).permit(:description)
-  end
-
   def set_vars
     @post = Post.find(params[:post_id])
     @gpt_creation = @post.gpt_creation
   end
+
+  def description_params
+    params.require(:gpt_creation).permit(:description)
+  end
+
+  def turbo_render(post, gpt_creation)
+    respond_to do |f|
+      f.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.replace(gpt_creation, partial: "gpt_creations/recreate", locals: { gpt_creation: gpt_creation }),
+          turbo_stream.replace("insta-#{gpt_creation.id}" , partial: "gpt_creations/insta-description", locals: { gpt_creation: gpt_creation, description: gpt_creation.description })
+        ]
+      end
+      f.html do
+        redirect_to post_path(post)
+      end
+    end
+  end
+
 end
