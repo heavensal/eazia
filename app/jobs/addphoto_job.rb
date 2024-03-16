33c17eb1732671cd463472f4b03b4f2052bf08f5
adam lamouri <adamlamouri.pro@gmail.com>
2024-03-16 13:49:17 +0100

    loading ophoto

--- app/jobs/addphoto_job.rb
+++ app/jobs/addphoto_job.rb
@@ -3,18 +3,25 @@ class AddphotoJob < ApplicationJob
 
   def perform(post_id)
     post = Post.find_by(id: post_id)
+    broadcast_loading_photo(post)
     ai_api_service = AiApiService.new(post.user)
     photo = ai_api_service.image(post)
     select_photos(post)
-    broadcast_photos(post)
+    broadcast_one_photo(post)
   end
 
-  def broadcast_photos(post)
+  def broadcast_loading_photo(post)
+    Turbo::StreamsChannel.broadcast_before_to post,
+        target: "create-load-photo",
+        partial: "photos/loading-photo"
+  end
+
+  def broadcast_one_photo(post)
     photos_selected = post.photos_selected.map do |id|
       post.photos.where(id: post.photos_selected).detect { |photo| photo.id == id.to_i }
     end.compact
-    Turbo::StreamsChannel.broadcast_append_to post,
-        target: "photos",
+    Turbo::StreamsChannel.broadcast_update_to post,
+        target: "loading-photo",
         partial: "photos/show-photo",
         locals: { photo: post.photos.last, post: post }
     Turbo::StreamsChannel.broadcast_update_to post,
